9a35c58c0df0719f84300024ab301d0a8b46123c
Ciel-247 <ciel_247@hotmail.com>
2017-11-23 16:46:02 +0800

    11-22 完成了correlations_parse方法，对每条用例的关联进行匹配处理，在数据库里新建一行解析过后的可能存在关联变量的列，用于后续请求的发送

--- TestCase/testcase_func.py
+++ TestCase/testcase_func.py
@@ -7,12 +7,14 @@ from sqlalchemy import Column, String, create_engine, MetaData,Table
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///E:\\GitHubProject\\InterfaceTest\\data\\AutoTest.db', echo = True)
+engine = create_engine('sqlite:///E:\\GitHubProject\\InterfaceTest\\data\\AutoTest.db', echo = False)
 metadata = MetaData(engine)
 DBsession = sessionmaker(engine)
 
 def test_execute(caseId):
-    pass
+    session = DBsession()
+    headers = session.query(test_case_data.headers).filter(test_case_data.caseId == caseId).one()[0]
+    print("headers is : ",headers)
 
 def import_case_into_sqlite(PATH,sheet_name):
     #首先连接数据库定义表
@@ -45,14 +47,18 @@ def import_case_into_sqlite(PATH,sheet_name):
             sheet_name = sheet_name,
             caseId = row_cont[0].value,
             class_name = row_cont[1].value,
-            func_name=row_cont[2].value,
-            caseDescription=row_cont[3].value,
-            http_method=row_cont[4].value,
-            uri=row_cont[5].value,
-            headers=row_cont[6].value,
-            body=row_cont[7].value,
-            kwassert=row_cont[8].value,
-            correlations=row_cont[9].value,
+            func_name = row_cont[2].value,
+            caseDescription = row_cont[3].value,
+            http_method = row_cont[4].value,
+            uri = row_cont[5].value,
+            headers = row_cont[6].value,
+            parsed_headers = None,
+            body = row_cont[7].value,
+            parsed_body = None,
+            kwassert = row_cont[8].value,
+            parsed_kwassert = None,
+            correlations = row_cont[9].value,
+            parsed_correlations = None,
             if_exec = row_cont[10].value,
             create_time = datetime.datetime.now()
         )
@@ -64,8 +70,57 @@ def import_case_into_sqlite(PATH,sheet_name):
     # print([c.name for c in test_case_table.columns])
     # print([c.name for c in result_table.columns])
 
-def parse_correlation():
-    pass
+def parse_correlation(sheet_name, caseId, response):
+    session = DBsession()
+    correlations = session.query(test_case_data).filter(test_case_data.sheet_name == sheet_name, test_case_data.caseId == caseId).one().correlations#根据sheet_name,caseId从数据库查找对应的correlations
+    # print("correlations is :",correlations)
+    correlations_list = correlations.split("&")#一条用例有多个关联时，通过"&"字符进行分割
+    # print(correlations_list)
+    correlations_dict = {}#定义一个dict用于存放每个关联，关联名作为key，对应正则表达式作为value
+    for item in correlations_list:#通过"="分割每个关联，得到关联dict
+        key = item.split("=")[0]
+        value = item.split("=")[1]
+        correlations_dict[key] = value
+    # print(correlations_dict)
+    #对dict中的每个关联，遍历该sheet_name下所有的headers,body,kwassert,correlations,如果含有%{key}格式的字符串，则替换为该key对应的value，放入对应parsed_xx列（不要直接修改原有列，这样可以从数据库分析用例的整体过程）
+    # <editor-fold desc="各列待替换caseId的dict定义">
+    parsing_headers_caseId = {}
+    parsing_body_caseId = {}
+    parsing_kwassert_caseId = {}
+    parsing_correlations_caseId = {}
+    # </editor-fold>
+    for key in correlations_dict:#对关联dict里面的每一个key去拿到存在key关键字的用例id（相关的四列分别有一个dict，key是关联名，value是caseId，供后续替换）
+        #因为使用session.query直接查出来的结果是由tuple组成的list【[(xxxx1,),(xxxx2,)]这样的】，所以用[caseId[0] for caseId in xxx]的方式分别取出tuple的第一个值
+        # print(key)
+        parsing_headers_caseId[key] = [caseId[0] for caseId in session.query(test_case_data.caseId).filter(test_case_data.headers.like("%%{"+key+"}%")).all()]
+        parsing_body_caseId[key] = [caseId[0] for caseId in session.query(test_case_data.caseId).filter(test_case_data.body.like("%%{"+key+"}%")).all()]
+        parsing_kwassert_caseId[key] = [caseId[0] for caseId in session.query(test_case_data.caseId).filter(test_case_data.kwassert.like("%%{"+key+"}%")).all()]
+        parsing_correlations_caseId[key] = [caseId[0] for caseId in session.query(test_case_data.caseId).filter(test_case_data.correlations.like("%%{" + key + "}%")).all()]
+    # print(parsing_headers_caseId,parsing_body_caseId)
+    # print("parsing_headers_caseId is : ", parsing_headers_caseId)
+    for key in correlations_dict:#对关联dict中的每一个关键字都要处理
+        replace_str = "%{" + key + "}"#用于正则表达式的匹配，用例中所有需要替换的部分都采取“%{key}”的方式来写的
+        newpat = ''.join(re.findall(r"%s" % correlations_dict[key] ,response))#用于替换的新词，从response中通过correlations_dict[key]的正则表达式来取出
+        for value in parsing_headers_caseId[key]:#对所有待替换headers的caseId进行处理
+            before = session.query(test_case_data.headers).filter(test_case_data.caseId == value).one()[0]
+            after = re.sub(r"%s" % replace_str, newpat, before)
+            session.query(test_case_data).filter(test_case_data.caseId == value ).update({test_case_data.parsed_headers : after})
+            session.commit()
+        for value in parsing_body_caseId[key]:#对所有待替换body的caseId进行处理
+            before = session.query(test_case_data.body).filter(test_case_data.caseId == value).one()[0]
+            after = re.sub(r"%s" % replace_str, newpat, before)
+            session.query(test_case_data).filter(test_case_data.caseId == value).update({test_case_data.parsed_body: after})
+            session.commit()
+        for value in parsing_kwassert_caseId[key]:#对所有待替换kwassert的caseId进行处理
+            before = session.query(test_case_data.kwassert).filter(test_case_data.caseId == value).one()[0]
+            after = re.sub(r"%s" % replace_str, newpat, before)
+            session.query(test_case_data).filter(test_case_data.caseId == value).update({test_case_data.parsed_kwassert: after})
+            session.commit()
+        for value in parsing_correlations_caseId[key]:#对所有待替换correlations的caseId进行处理
+            before = session.query(test_case_data.correlations).filter(test_case_data.caseId == value).one()[0]
+            after = re.sub(r"%s" % replace_str, newpat, before)
+            session.query(test_case_data).filter(test_case_data.caseId == value).update({test_case_data.parsed_correlations: after})
+            session.commit()
 
 def get_checkPoint(sheet_name, caseId):
     session = DBsession()
@@ -90,13 +145,17 @@ class test_case_data(Base):#test_case表定义表结构，用于直接将dict插
     http_method = Column(String)
     uri = Column(String)
     headers = Column(String)
+    parsed_headers = Column(String)
     body= Column(String)
+    parsed_body= Column(String)
     kwassert = Column(String)
+    parsed_kwassert = Column(String)
     correlations = Column(String)
+    parsed_correlations = Column(String)
     if_exec = Column(String)
     create_time = Column(String)
 
-    def __init__(self, sheet_name, caseId, class_name, func_name, caseDescription, http_method, uri, headers, body, kwassert, correlations, if_exec, create_time):
+    def __init__(self, sheet_name, caseId, class_name, func_name, caseDescription, http_method, uri, headers, parsed_headers, body, parsed_body, kwassert, parsed_kwassert, correlations, parsed_correlations, if_exec, create_time):
         self.sheet_name = sheet_name
         self.caseId = caseId
         self.class_name = class_name
@@ -105,9 +164,13 @@ class test_case_data(Base):#test_case表定义表结构，用于直接将dict插
         self.http_method = http_method
         self.uri = uri
         self.headers = headers
+        self.parsed_headers = parsed_headers
         self.body = body
+        self.parsed_body = parsed_body
         self.kwassert = kwassert
+        self.parsed_kwassert = parsed_kwassert
         self.correlations = correlations
+        self.parsed_correlations = parsed_correlations
         self.if_exec = if_exec
         self.create_time = create_time
 
@@ -115,3 +178,7 @@ if __name__ == "__main__":
     import_case_into_sqlite("../data/test-case_v1116_v2.xlsx","suites-test_zj_ysbqc")
 
     get_checkPoint("suites-test_zj_ysbqc","YSBQC01_1")
+
+    parse_correlation("suites-test_zj_ysbqc","YSBQC01_4",'''"dzswj_tgc": "1234""''')
+
+    test_execute("YSBQC01_2")
